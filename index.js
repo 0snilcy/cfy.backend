e36acd5912a54ece93fe57133c75b0e339cf3b20
Johnny Rocket <0.snilcy@gmail.com>
2020-01-24 02:36:02 +0300

    Fix development env

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-require('module-alias/register')
 const path = require('path')
 
 require('dotenv').config({
 	path: path.resolve(__dirname, './env/', '.env.' + process.env.NODE_ENV),
 })
+
+require('./src/server').start()
--- src/db/connection.js
+++ src/db/connection.js
@@ -5,15 +5,15 @@ const logger = require('debug')('components:db:')
 const connect = async () => {
 	if (NODE_ENV !== 'test') {
 		try {
+			mongoose.connection.on('connected', () =>
+				logger('Mongoose default connection open to ' + DB_URL)
+			)
+
 			await mongoose.connect(DB_URL, {
 				useNewUrlParser: true,
 				useUnifiedTopology: true,
 				useCreateIndex: true,
 			})
-
-			mongoose.connection.on('connected', () =>
-				logger('Mongoose default connection open to ' + DB_URL)
-			)
 		} catch (err) {
 			logger(err.message)
 			process.exit(1)
